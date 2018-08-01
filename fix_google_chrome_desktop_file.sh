1a5671efb0eb1abf3a92d66273cd3e0cbb67ade5
Shriniwas <shriniwas26@gmail.com>
2018-08-01 11:48:21 +0200

    Make the fix idempotent

--- fix_google_chrome_desktop_file.sh
+++ fix_google_chrome_desktop_file.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
 # Fix google chrome flickering issue
+DESKTOP_FILE='/usr/share/applications/google-chrome.desktop'
+NUM_MATCHES=$(grep "disable-gpu-driver-bug-workarounds.*enable-native-gpu-memory-buffers" $DESKTOP_FILE | wc -l)
+echo "$NUM_MATCHES matches found"
+if (( $NUM_MATCHES >= 3 ))
+then
+    echo "Fix already applied"
+else
+    echo "Applying changes to  $DESKTOP_FILE";
+    sed -i 's/google-chrome-stable/google-chrome-stable --disable-gpu-driver-bug-workarounds --enable-native-gpu-memory-buffers /g' $DESKTOP_FILE
+fi
 
-sed -i 's/google-chrome-stable/google-chrome-stable --disable-gpu-driver-bug-workarounds --enable-native-gpu-memory-buffers /g' /usr/share/applications/google-chrome.desktop
+
