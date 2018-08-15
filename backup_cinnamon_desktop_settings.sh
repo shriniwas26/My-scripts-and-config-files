82825683d97676c982c3f5b8e37fc7d9c851aab9
Shriniwas Sharma <shriniwas26@gmail.com>
2018-08-15 14:43:04 +0200

    Better backup script

--- backup_cinnamon_desktop_settings.sh
+++ backup_cinnamon_desktop_settings.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Backup all Cinnamon DE Related settings
 # Run periodically via cron
-FOLDER="/media/shriniwas/MyData/SURFdrive/Backups/Cinnamon_Desktop_Settings/$(date +"%Y_%m")"
-mkdir -p $FOLDER
+BACKUP_FOLDER="/media/shriniwas/MyData/SURFdrive/Backups/Cinnamon_Desktop_Settings/$(date +"%Y_%m")"
+mkdir -p $BACKUP_FOLDER
 ROTATE_DAYS=20
-zip -rv "$FOLDER/linux_mint_config_$(date +"%Y_%m_%d_%H%M").zip" ~/.atom ~/.config ~/.cinnamon ~/.gconf ~/.icons ~/.themes -x \*spotify* \*chromium* \*google-chrome* \*Virtualbox* \*Cache* \*IndexedDB*
-FILE_COUNT=`find $FOLDER -maxdepth 1 -type f | wc -l`
+zip -rv "$BACKUP_FOLDER/linux_mint_config_$(date +"%Y_%m_%d_%H%M").zip" ~/.atom/*.cson ~/.config ~/.cinnamon ~/.gconf ~/.icons ~/.themes -x \*libreoffice* \*spotify* \*chromium* \*google-chrome* \*Virtualbox* \*Cache* \*IndexedDB*
+FILE_COUNT=`find $BACKUP_FOLDER -maxdepth 1 -type f | wc -l`
 echo "Backup Folder has $FILE_COUNT backups, removing backups older than $ROTATE_DAYS days"
-find $FOLDER -mtime +$ROTATE_DAYS -exec rm {} \;
+find $BACKUP_FOLDER -mtime +$ROTATE_DAYS -exec rm {} \;
