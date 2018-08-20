45280a3ad14d31fd46620f2ca6bc550ddaee6a6f
Shriniwas Sharma <shriniwas26@gmail.com>
2018-08-20 15:09:39 +0200

    Backupn additional files

--- backup_cinnamon_desktop_settings.sh
+++ backup_cinnamon_desktop_settings.sh
@@ -4,7 +4,7 @@
 BACKUP_FOLDER="/media/shriniwas/MyData/SURFdrive/Backups/Cinnamon_Desktop_Settings/$(date +"%Y_%m")"
 mkdir -p $BACKUP_FOLDER
 ROTATE_DAYS=20
-zip -rv "$BACKUP_FOLDER/linux_mint_config_$(date +"%Y_%m_%d_%H%M").zip" ~/.atom/*.cson ~/.config ~/.cinnamon ~/.gconf ~/.icons ~/.themes -x \*libreoffice* \*spotify* \*chromium* \*google-chrome* \*Virtualbox* \*Cache* \*IndexedDB*
+zip -rv "$BACKUP_FOLDER/linux_mint_config_$(date +"%Y_%m_%d_%H%M").zip" ~/.bashrc ~/.bash_it ~/.atom/*.cson ~/.config ~/.cinnamon ~/.gconf ~/.icons ~/.themes -x \*.git* \*libreoffice* \*spotify* \*chromium* \*google-chrome* \*Virtualbox* \*Cache* \*IndexedDB*
 FILE_COUNT=`find $BACKUP_FOLDER -maxdepth 1 -type f | wc -l`
 echo "Backup Folder has $FILE_COUNT backups, removing backups older than $ROTATE_DAYS days"
 find $BACKUP_FOLDER -mtime +$ROTATE_DAYS -exec rm {} \;
