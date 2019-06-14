df0e96b5b7184e6a0c9fbac1f4f1d05ddf0614fc
Shriniwas Sharma <shriniwas26@gmail.com>
2019-06-14 17:48:51 +0200

    Shellcheck fixes

--- backup_cinnamon_desktop_settings.sh
+++ backup_cinnamon_desktop_settings.sh
@@ -2,9 +2,10 @@
 # Backup all Cinnamon DE Related settings
 # Run periodically via cron
 BACKUP_FOLDER="/media/shriniwas/MyData/SURFdrive/Backups/Cinnamon_Desktop_Settings/$(date +"%Y_%m")"
-mkdir -p $BACKUP_FOLDER
+mkdir -p "$BACKUP_FOLDER"
 ROTATE_DAYS=20
+
 zip -rv "$BACKUP_FOLDER/linux_mint_config_$(date +"%Y_%m_%d_%H%M").zip" ~/.bashrc ~/.bash_it ~/.atom/*.cson ~/.config ~/.cinnamon ~/.gconf ~/.icons ~/.themes -x \*.git* \*libreoffice* \*spotify* \*chromium* \*google-chrome* \*Virtualbox* \*Cache* \*IndexedDB*
-FILE_COUNT=`find $BACKUP_FOLDER -maxdepth 1 -type f | wc -l`
+FILE_COUNT=$(find "$BACKUP_FOLDER" -maxdepth 1 -type f | wc -l)
 echo "Backup Folder has $FILE_COUNT backups, removing backups older than $ROTATE_DAYS days"
-find $BACKUP_FOLDER -mtime +$ROTATE_DAYS -exec rm {} \;
+find "$BACKUP_FOLDER" -mtime +$ROTATE_DAYS -exec rm {} \;
--- fix_google_chrome_desktop_file.sh
+++ fix_google_chrome_desktop_file.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Fix google chrome flickering issue
 DESKTOP_FILE='/usr/share/applications/google-chrome.desktop'
-NUM_MATCHES=$(grep "disable-gpu-driver-bug-workarounds.*enable-native-gpu-memory-buffers" $DESKTOP_FILE | wc -l)
+NUM_MATCHES=$(grep -c "disable-gpu-driver-bug-workarounds.*enable-native-gpu-memory-buffers" "$DESKTOP_FILE")
 echo "$NUM_MATCHES matches found"
-if (( $NUM_MATCHES >= 3 ))
+
+if (( "$NUM_MATCHES" > 3 ));
 then
     echo "Fix already applied"
 else
