71e0ecc53510360ade4fb4e823cd0d51331747b8
Shriniwas <shriniwas26@gmail.com>
2021-01-19 18:10:40 +0100

    Minor changes to retry.py

--- retry.py
+++ retry.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
+import os
+import subprocess
 import sys
 import time
-import subprocess
 
 RETRY_DELAY = 1
 MAX_ATTEMPTS = 20
+
 
 def main(args):
     for i in range(1, MAX_ATTEMPTS+1):
         try:
             shellout = subprocess.run(args)
             if shellout.returncode == 0:
-                break
+                return 0
             else:
                 raise RuntimeError("Nonzero return code!")
         except Exception as e:
@@ -21,11 +23,14 @@ def main(args):
             print("## Retrying in {}s ...".format(RETRY_DELAY))
             print("#" * 25)
             time.sleep(RETRY_DELAY)
-    return 0
+
+    print("## All {} attempts over. Exiting.".format(MAX_ATTEMPTS))
+    return 1
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2 or sys.argv[1] in ['-h', '--help']:
-        print("Usage: run-forever <Executable> [<Command-line options>]")
+        self_name = os.path.split(sys.argv[0])[1]
+        print("Usage: {} <Executable> [CMD LINE ARG 1] [CMD LINE ARG 2] ...".format(self_name))
         sys.exit(1)
     sys.exit(main(sys.argv[1:]))
